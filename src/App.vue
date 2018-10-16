87245bd33770543d4a8926e4f0f9bdd57b2367b1
ljy <ljy1024>
2018-10-16 16:12:08 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+    <Bind></Bind>
+    <User></User>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import Bind from './components/Bind'
+import User from './components/User'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Bind
+    User
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
